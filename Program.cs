7b01f5f66e5e858f18f9fdb634e98d7195d00081
Atul Mirajkar <amirajka@epic.com>
2022-12-19 16:42:31 -0600

    add argument

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System;
 using System.Text;
 
 namespace typing_gen
@@ -8,20 +9,28 @@ namespace typing_gen
         static void Main(string[] args)
         {
             string input = "!@#$%^&*()_+[]{}\\|;:'\",<.>/?";
+            int cnt = input.Length;
+            if(args.Length!=0){
+                cnt = Convert.ToInt32(args[0]);
+            }
+
             Random r = new Random();
             Console.WriteLine();
             StringBuilder sbInput = new StringBuilder();
-            for(int j=0; j<input.Length; j++){
+            for(int j=0; j<cnt; j++){
                 int idx = r.Next()%input.Length;
                 sbInput.Append(input[idx]);
             }
-
             Console.WriteLine(sbInput);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
             string user = Console.ReadLine();
+            sw.Stop();
 
             StringBuilder sb = new StringBuilder();
             int i=0;
-            while(i<user.Length){
+            while(i<user.Length && i<sbInput.Length){
                 if(user[i] == sbInput[i]){
                     i++;
                     continue;
@@ -34,7 +43,8 @@ namespace typing_gen
                 i++;
             }
 
-            Console.WriteLine(sb);
+            Console.WriteLine($"Typos: {sb}");
+            Console.WriteLine($"Total Time: {sw.ElapsedMilliseconds/1000} seconds");
         }
     }
 }
